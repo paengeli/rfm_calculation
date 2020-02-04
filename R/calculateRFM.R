5ef2310a786f36350a09d4940bed9288ced1889e
Pascal Engeli <pascal.engeli@uzh.ch>
2020-02-04 12:50:14 +0100

    initial commit

---
+++ R/calculateRFM.R
@@ -0,0 +1,63 @@
+#' calculateRFM
+#'
+#'Description
+#'   Calculate a weighted RFM score: recency, frequency, and monetary value for every customer
+#'
+#'Arguments
+#'@param data - A data.table containing the transaction recorded details for every customer.
+#'@param r - Weight of recency
+#'@param f - Weight of frequency
+#'@param m - Weight of monetary value
+#'
+#'
+
+
+
+
+
+RFMfunction <- function(data,
+                        weight_recency=1,
+                        weight_frequency=1,
+                        weight_monetary=1){
+
+  # Load libraries with require()
+  require(data.table)
+  require(lubridate)
+  require(Hmisc)
+
+  # Ensure that the weights add up to one
+  weight_recency2 <- weight_recency/sum(weight_recency,
+                                        weight_frequency, weight_monetary)
+  weight_frequency2 <- weight_frequency/sum(weight_recency,
+                                            weight_frequency, weight_monetary)
+  weight_monetary2 <- weight_monetary/sum(weight_recency,
+                                          weight_frequency, weight_monetary)
+
+  # RFM measures
+  max.Date <- max(data$TransDate)
+  temp <- data[,list(
+    recency = as.numeric(max.Date - max(TransDate)),
+    frequency = .N,
+    monetary = sum(PurchAmount)/.N),
+    by="Customer"
+    ]
+
+  # RFM scores
+  temp <- temp[,list(Customer,
+                     recency = as.numeric(cut2(-recency, g=3)),
+                     frequency = as.numeric(cut2(frequency, g=3)),
+                     monetary = as.numeric(cut2(monetary, g=3)))]
+
+  # Overall RFM score
+  temp[,finalscore:=weight_recency2*recency+
+         weight_frequency2*frequency+
+         weight_monetary2*monetary,
+       by=Customer]
+
+  # RFM group
+  temp[,group:=round(finalscore)]
+
+  # Return final table
+  return(temp)
+}
+
